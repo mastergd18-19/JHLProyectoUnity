fe58901932803ab93a59c9d1eeb32a711e85ac7f
Dallgo <juan.hidalgo@live.u-tad.com>
2019-05-07 21:44:17 +0200

    Clase 07/05/2019 Movimiento automático y cámara 2d

---
+++ Assets/Scripts/playerMovement_Translation.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_Translation : MonoBehaviour
+{
+    public float speed;
+    public bool moving;
+    public bool direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            direction = !direction;
+            
+        }
+
+    }
+
+    void Movement()
+    {
+        //float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+
+        if (direction)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else
+        {
+
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+        }
+       
+       
+        /*else
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }*/
+
+        /*if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        */
+    }
+}
