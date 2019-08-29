3382503cdebd0b9ac0eaf5b794e2886f33482a2c
Dallgo <juaneshl1@gmail.com>
2019-08-29 16:45:10 +0200

    fix menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+
+    public Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -15,4 +18,7 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+
+
 }
--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,15 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame ()
+
+    public void PlayGame ()
     {
+        Debug.Log("LOooooooooooL");
+
         SceneManager.LoadScene("Game");
     }
 
 
     public void QuitGame ()
     {
+
+
+
+#if UNITY_EDITOR
+        Debug.Log("Unity Editor");
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
+
+
+#if UNITY_STANDALONE_WIN
+        Debug.Log("Stand Alone Windows");
         Debug.Log("Se ha cerrado");
         Application.Quit();
+#endif    
+        
+
     }
 }
