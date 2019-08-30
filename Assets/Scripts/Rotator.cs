b3f8322d0d0fa9c45e0a1cc0c65db2a3ef769df2
Dallgo <juan.hidalgo@live.u-tad.com>
2019-08-30 18:37:47 +0200

    Backup

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float Tumble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.angularvelocity = Random.insideUnitSphere * Tumble;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
